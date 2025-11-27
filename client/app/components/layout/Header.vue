761dafd6d5377af37b2e6e430fd2672f855bba47
Daniel <daniel.barquero.dev@gmail.com>
2025-11-27 10:28:46 -0600

    Update Header layout and add logo and icons
    Refactored the Header component to improve layout and styling. Added a logo link and user/account icons, and passed isMobile prop to notification inbox. Adjusted class names for better responsiveness and alignment.

--- client/app/components/layout/Header.vue
+++ client/app/components/layout/Header.vue
@@ -2,7 +2,7 @@
 
 const props = defineProps<{
     sidenavOpen: boolean
-
+    isMobile: boolean
 }>()
 
 //trigger update props
@@ -14,9 +14,21 @@ const emit = defineEmits([
 <template>
     <nav class="fixed top-0 w-full h-12 z-50 transition-all duration-400">
 
-        <div :class="props.sidenavOpen ? 'lg:ps-60' : 'ps-4 lg:ps-15'"
-            class="flex items-center w-full h-full rounded-t-lg bg-gray-800 shadow-sm relative">
-            HEADER
+        <div :class="props.sidenavOpen ? 'lg:pl-60' : 'sm:px-12 sm:mx-4'"
+            class="flex items-center justify-between w-full h-full bg-gray-800 shadow-sm px-4 transition-all duration-400">
+
+            <!-- Logo -->
+            <NuxtLink to="/" class="flex items-center justify-start gap-2 text-white hover:text-gray-300">
+                <span
+                    class="bg-linear-to-r from-blue-500 to-rose-500 bg-clip-text font-bold tracking-tight text-transparent text-2xl">OVIS</span>
+            </NuxtLink>
+
+            <!-- Icons -->
+            <div class="flex items-center gap-4">
+                <layout-notification-inbox :isMobile="props.isMobile" />
+                <icon name="material-symbols:account-circle-full" size="26"
+                    class="text-gray-300 hover:text-white cursor-pointer transition-colors" />
+            </div>
         </div>
 
     </nav>
