27091e39e95a2977945a9aaefec5fd2c6aa36471
Daniel <daniel.barquero.dev@gmail.com>
2025-12-04 17:38:10 -0600

    Refactor user profile routes and add index page
    Moved player profile components to a unified user profile path for better organization. Added a new index.vue for the user profile, displaying user stats with a hardcoded UUID.

---
+++ client/app/pages/user/profile/[uuid]/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+useHead({
+    title: 'Mi cuenta' //Aplicar i18n
+});
+
+//const route = useRoute();
+const uuid = '12345678-1234-1234-1234-123456789012'; //hardcoded uuid
+// url = http://localhost:3000/user/profile/12345678-1234-1234-1234-123456789012;
+
+
+</script>
+<template>
+
+    <div class="flex w-full">
+        <user-player-stats :uuid="uuid" />
+    </div>
+
+</template>
